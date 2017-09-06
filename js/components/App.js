da7bd6e5e272a2a283c35fe45380e1272bfd819f
iamglenbacs <gpbaculio@gmail.com>
2017-09-06 22:25:55 +0800

    ready to deploy

--- js/components/App.js
+++ js/components/App.js
@@ -1,79 +1,118 @@
+import 'babel-polyfill'
 import React, { Component } 
-        from 'react';
-import $          from 'jquery';
-import { Button } from 'semantic-ui-react'
-import 'jquery-ui'
+  from 'react';
+import $ from 'jquery';
+import 'jquery-ui';
 
-class Article extends Component {
-_renderSnippet = () => {
-  return { __html:this.props.article.snippet };
-}
-
+class Channel extends Component {
+  state = {
+    game:'',
+    online:'offline',
+    displayName:'',
+    logo: '',
+    status: '',
+  }
+  componentWillMount = async () => {
+    const { channelData } = this.props;
+    if (channelData.res.stream === null) {
+        await this.setState((prevState, props) => ({
+          game: 'Offline',
+          online: 'offline',
+          logo: channelData.data.logo,
+          displayName: channelData.data.display_name,
+          status: channelData.data.status
+        }));
+      } else if (channelData.res.stream === undefined) {
+        await this.setState((prevState, props) => ({
+          game: 'Account Closed',
+          online: 'offline',
+          logo: channelData.data.logo,
+          displayName: channelData.data.display_name,
+          status: channelData.data.status
+        }));
+      } else {
+        await this.setState((prevState, props) => {
+                return {
+                    game: channelData.res.stream.game,
+                    online: 'online',
+                    logo: channelData.data.logo,
+                    displayName: channelData.data.display_name,
+                    status: channelData.data.status
+              }
+            });
+      };
+  }
   render() {
     return (
-        <a style={{ color: 'black', textDecoration: 'none'}} className="articleLink" href={"https://en.wikipedia.org/wiki/"+this.props.article.title} target="_blank">
-          <div style={{ borderRadius: '3px', padding: '15px', marginTop: '15px' }} className="article"> 
-            <span style={{ fontSize: '18px' }} > {this.props.article.title} </span>
-            <div style={{textAlign: 'left', fontSize: '14px', marginTop: '12px'}} dangerouslySetInnerHTML={this._renderSnippet()}/>
-          </div>
-        </a>
-      )
+      <div style={Boolean(this.props.show === this.state.online || this.props.show === 'all' ) ? { borderRadius: '3px', padding: '5px', marginTop: '15px' } : {display: 'none'} } className="channel"> 
+        <div style={{ fontSize: '18px',textAlign:'center' }} className={Boolean(this.state.online==='online') ? "online" : ""}>
+          <table style={{width: '100%'}}>
+            <tr>
+              <td style={{padding:'5px',verticalAlign: 'middle', textAlign: 'center', marginLeft: '20px', marginRight: '20px'}}><img style={{width: '50px', height: '50px', borderRadius: '50%',textAlign: 'left', position: 'relative'}} src={this.state.logo} /></td>
+              <td style={{padding:'5px',verticalAlign: 'middle', textAlign: 'center', marginLeft: '20px', marginRight: '20px'}}><span style={{marginLeft:'15px', width: '100px', marginRight: '15px'}}> {this.state.displayName} </span></td>
+              <td style={{padding:'5px',verticalAlign: 'middle', textAlign: 'center', width: '250px', height:'100px',marginLeft: '20px', marginRight: '20px'}}><span style={{fontSize: '12px'}}>{Boolean(this.state.online === 'online') ? `${this.state.game} : ${this.state.status}`: 'Offline'}</span></td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    )
   }
 }
 class App extends Component {
 
   state = {
-    searchText:'',
-    searchResults: []
+    channelsData: [],
+    show: 'all'
   }
 
-  _search = (e) => {
-    // Use Ajax to handle things
-    e.preventDefault()
-    if(this.state.searchText !== '') {
-      let setState = (result) => {
-        this.setState((prevState, props) => ({
-            searchResults: result.query.search
-        }));
+  componentWillMount() {
+    let channels = ["freecodecamp","ns_vp","ESL_SC2"];
+      let setState = async (result) => {
+        await this.setState((prevState, props) => ({
+                  channelsData:[...prevState.channelsData,result]
+                }));
       }
-      $.ajax({
-        url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=' + this.state.searchText.toString(),
-        dataType: 'jsonp',
-        type: 'POST',
-        headers: {
-          'Api-User-Agent': 'Example/1.0'
-        },
-        success: function(data) {
-          console.log("data = ", data)
-          setState(data)  
-        }
-      });
-    }
+    channels.forEach(function(channel) {
+      function genUrl(type, name) {
+        return 'https://wind-bow.gomix.me/twitch-api/' + type + '/' + name + '?callback=?';
+      }
+        $.ajax({ // 2 ajax requests
+          url: genUrl("channels", channel),
+          async: false,
+          dataType: 'jsonp',
+          success: async function(data) {
+            debugger;
+            $.ajax({
+              url: genUrl("streams", channel),
+              async: false,
+              dataType: 'jsonp',
+              success: async function(res) { // res = result
+                debugger;
+                console.log("data+res = ",{data, res})
+                await setState({data, res});
+              },
+            });
+          },
+        });
+    });
   }
 
-  _handleInputChange = (e) => {
-    this.setState({searchText: e.target.value});
-  };
+  _renderChannels() {
+     return this.state.channelsData.map((channelData, i) => {
+          return <Channel show={this.state.show} channelData={channelData} />
+      })
+    };
 
-  _renderArticles() {
-    return this.state.searchResults.map(searchResult =>
-      <Article
-        article={searchResult}
-      />
-    );
-  }
   render() {
     return (
         <div> 
-          <span className="title"> Reactjs Show the Local Weather </span>
+          <span className="title"> Reactjs Twitchtv JSON API </span>
             <div className="container">
-              <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank">
-                <button type="button">Random Article</button>
-              </a>
-              <div style={{fontSize: '20px', marginTop:'15px'}}> 
-                <form  onSubmit={this._search}><input placeholder={'Search Wikipedia'} onChange={this._handleInputChange} /></form>
-                <button onClick={(e) => this._search(e)}> Search </button></div>
-              {this._renderArticles()}
+            <span> Twitch Streamers </span>
+            <button style={{margin:'5px', padding: '3px'}} onClick={() => this.setState({ show: 'all'})} >All</button>
+            <button style={{margin:'5px', padding: '3px'}} onClick={() => this.setState({ show: 'online'})} className={Boolean(this.state.show==='online')?'online' : ''} >Online</button>
+            <button style={{margin:'5px', padding: '3px'}} onClick={() => this.setState({ show: 'offline'})} className={Boolean(this.state.show==='offline')? 'offline' : ''}>Offline</button>
+              {this._renderChannels()}
               <div style={{marginTop: '40px'}}>
                 <span style={{float: 'left'}}> Deployed Heroku App: <a href="https://gpbaculio-wikipedia-viewer.herokuapp.com/"> link </a> </span>
                 <span style={{float: 'right'}}> Github Repo: <a href="https://github.com/iamglenbacs/gpbaculio-wikipedia-viewer"> link </a> </span>
